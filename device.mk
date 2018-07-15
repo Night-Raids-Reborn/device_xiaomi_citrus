65bb8b10d2e4c0b48d7034ad4de9919112cda702
Christian Oder <myself5@carbonrom.org>
2018-07-15 21:31:07 +0200

    citrus: Add basic permission file for HotwordEnrollment
    Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ PRODUCT_PACKAGES += \
 # Configuration files
 include $(LOCAL_PATH)/audio/audio.mk
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
