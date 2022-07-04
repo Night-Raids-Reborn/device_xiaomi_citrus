d45f120d170c0d4a10c41bf7edc28d37d920348d
Jabiyeff <cebiyevanar@gmail.com>
2022-07-04 11:41:54 +0700

    citrus: Strip com.qti.node.mialgocontrol.so
    * Size decreased 148 mb to 12 mb, its usable right now
    * Fixed video recording issue on GCAM and ANX camera apps
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Change-Id: Iffe32028ee91c96dd66af22794ab90d82741f523

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
+            llvm-strip --strip-debug "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
