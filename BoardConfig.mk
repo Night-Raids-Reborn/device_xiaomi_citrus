41fde0c06662488116cdcfdc6dd27d34fe2e42b0
SebaUbuntu <barezzisebastiano@gmail.com>
2023-01-17 02:52:30 +0700

    citrus: Add libinit for variants handling
    Change-Id: Ic01e729cdfd32cf7ca0cae477147e5f06d6ef8d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,10 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_citrus
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_citrus
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/citrus_defconfig
 
