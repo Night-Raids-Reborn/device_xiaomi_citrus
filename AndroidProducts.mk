aab0ce96afb0b667530f3d0ced2b83ac56326f4e
darknius09 <darkniuslightness@gmail.com>
2023-07-30 12:46:26 +0000

    Lineage Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_citrus.mk
+    $(LOCAL_DIR)/aosp_citrus.mk \
+    $(LOCAL_DIR)/lineage_citrus.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_citrus-user \
     aosp_citrus-userdebug \
-    aosp_citrus-eng
+    aosp_citrus-eng \
+    lineage_citrus-user \
+    lineage_citrus-userdebug \
+    lineage_citrus-eng
