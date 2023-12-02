efab88ed1e500904c7a43bb1c55db8f33efc5f31
darknius09 <darkniuslightness@gmail.com>
2023-12-02 08:03:06 +0000

    citrus: rm board info

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,11 +9,5 @@ DEVICE_PATH := device/xiaomi/citrus
 # Inherit from Xiaomi SM6115
 include device/xiaomi/chime/BoardConfigCommon.mk
 
-# Board
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
-
-# Display
-TARGET_SCREEN_DENSITY := 440
-
 # Inherit the proprietary files
 include vendor/xiaomi/citrus/BoardConfigVendor.mk
